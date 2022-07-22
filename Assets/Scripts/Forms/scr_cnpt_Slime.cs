f8c650c8590c6430afa3e695ecb682c36bffe039
lutoshkinstep@gmail.com <>
2022-07-22 16:51:26 +0700

    Half pipes update

--- Assets/Scripts/Forms/scr_cnpt_Slime.cs
+++ Assets/Scripts/Forms/scr_cnpt_Slime.cs
@@ -6,6 +6,8 @@ public class scr_cnpt_Slime : scr_cnpt_Form_Abstract
 {
     public float interactionRadius = 0.3f;
 
+    public bool isPipeCrawling = false;
+
     public scr_cnpt_Slime(scr_cnpt_FormBehavior formBehavior)
     {
         sprite = Resources.Load<Sprite>("Slime");
@@ -14,6 +16,7 @@ public class scr_cnpt_Slime : scr_cnpt_Form_Abstract
 
     public override void Skill_1()
     {
+        //сначала собирать массив, потом провер€ть на длину 0, »—ѕ–ј¬»“№
         if (GetInteractableObjects(formBehavior.gameObject.transform, interactionRadius, LayerMask.GetMask("InteractableObjects")).Length != 0)
         {
             Collider2D[] targets = GetInteractableObjects(formBehavior.gameObject.transform, interactionRadius, LayerMask.GetMask("InteractableObjects"));
@@ -36,7 +39,62 @@ public class scr_cnpt_Slime : scr_cnpt_Form_Abstract
 
     public override void Skill_2()
     {
-        Debug.Log("*Heal sound*");
+        Collider2D[] targets = GetInteractableObjects(formBehavior.gameObject.transform, interactionRadius, LayerMask.GetMask("Pipe"));
+        if (targets.Length != 0)
+        {
+            if (!isPipeCrawling)
+            {
+                StartPipeCrawling(targets[0].gameObject);
+            }
+            else
+            {
+                StopPipeCrawling(targets[0].gameObject);
+            } 
+        }
+    }
+
+    public override void Move(Rigidbody2D rb, Vector2 moveDirection, float moveSpeed, float movementSmoothing)
+    {
+        if (isPipeCrawling)
+        {
+            Vector2 velocity = Vector2.zero;
+            Vector2 targetVelocity;
+            rb.gravityScale = 0;
+            targetVelocity = new Vector3(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+        }
+        else
+        {
+            rb.gravityScale = 0.65f;
+            base.Move(rb, moveDirection, moveSpeed, movementSmoothing);
+        }
+        
+    }
+
+    public override void Jump(Rigidbody2D rb, float jumpPower)
+    {
+        if (!isPipeCrawling)
+        {
+            base.Jump(rb, jumpPower);
+        }
+    }
+
+    public void StartPipeCrawling(GameObject pipe)
+    {
+        isPipeCrawling = true;
+        Transform entrance = pipe.transform.GetChild(0);
+        transform.position = entrance.transform.position;
+        //change sprite
+        //change collider
+    }
+
+    public void StopPipeCrawling(GameObject pipe)
+    {
+        isPipeCrawling = false;
+        Transform exit = pipe.transform.GetChild(1);
+        transform.position = exit.transform.position;
+        //change sprite
+        //change collider
     }
 
     public override void StopUsingCurrentForm()
